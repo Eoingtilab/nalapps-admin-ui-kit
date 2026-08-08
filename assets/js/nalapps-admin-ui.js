0b1cc24feee6be42ab59778ba7659240292369fb
Eoingti Lab Inc. <master@eoingti.com>
2026-08-09 03:20:09 +0900

    Add shared admin script

---
+++ assets/js/nalapps-admin-ui.js
@@ -0,0 +1,7 @@
+/* NalApps Admin UI Kit v2.0
+ * Intentionally minimal. Product-specific behavior belongs to each plugin.
+ */
+(function () {
+  'use strict';
+  document.documentElement.classList.add('nalapps-admin-ui-ready');
+})();
