fc2432a8ee6b27924c45017182b6c3238fc75663
Eoingti Lab Inc. <master@eoingti.com>
2026-08-09 14:12:00 +0900

    Fix final WPCS class closing layout

--- tools/scaffold_plugin.py
+++ tools/scaffold_plugin.py
@@ -126,6 +126,7 @@ def plugin_class(profile: dict, company: dict) -> str:
 \t\tdo_action( '{hook}' );
 \t}}
 '''
+    class_close = "}" if cron_method else "\n}"
 
     return f'''<?php
 /**
@@ -167,7 +168,7 @@ final class Plugin {{
 \t\t$links[] = '<a href="{repo}" target="_blank" rel="noopener noreferrer">GitHub</a>';
 \t\t$links[] = '<a href="' . esc_url( admin_url( 'options-general.php?page={slug}-system-status' ) ) . '">System Status</a>';
 \t\treturn $links;
-\t}}{cron_method}}}
+\t}}{cron_method}{class_close}
 '''
 
 
