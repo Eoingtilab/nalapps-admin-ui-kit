34e69a08f65d0a1a435d0aebeba4cb30b135c4b7
Eoingti Lab Inc. <master@eoingti.com>
2026-08-10 05:22:39 +0900

    Generate verified release rollback center

--- tools/maintenance_rollback.py
+++ tools/maintenance_rollback.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Generate NalApps pre-update code backup and rollback runtime."""
+"""Generate NalApps pre-update backup plus verified Release Asset rollback runtime."""
 from __future__ import annotations
 
 from scaffold_plugin import namespace_suffix, php_const_prefix
@@ -25,10 +25,12 @@ if ( ! defined( 'ABSPATH' ) ) {{
 
 final class Rollback_Manager {{
 \tconst MAX_BACKUPS = 3;
+\tconst RELEASES_API = 'https://api.github.com/repos/Eoingtilab/{slug}/releases?per_page=30';
 
 \tpublic function __construct() {{
 \t\tadd_filter( 'upgrader_pre_install', array( $this, 'backup_before_update' ), 10, 2 );
 \t\tadd_action( 'admin_post_{action}_rollback', array( $this, 'rollback' ) );
+\t\tadd_action( 'admin_post_{action}_release_rollback', array( $this, 'rollback_release' ) );
 \t}}
 
 \tpublic static function backup_directory() {{
@@ -89,6 +91,53 @@ final class Rollback_Manager {{
 \t\treturn $names;
 \t}}
 
+\tpublic static function list_release_versions() {{
+\t\t$cache_key = '{action}_release_rollback_versions';
+\t\t$cached    = get_transient( $cache_key );
+\t\tif ( is_array( $cached ) ) {{
+\t\t\treturn $cached;
+\t\t}}
+\t\t$response = wp_remote_get(
+\t\t\tself::RELEASES_API,
+\t\t\tarray(
+\t\t\t\t'timeout' => 10,
+\t\t\t\t'headers' => array( 'Accept' => 'application/vnd.github+json' ),
+\t\t\t)
+\t\t);
+\t\tif ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {{
+\t\t\treturn array();
+\t\t}}
+\t\t$releases = json_decode( wp_remote_retrieve_body( $response ), true );
+\t\tif ( ! is_array( $releases ) ) {{
+\t\t\treturn array();
+\t\t}}
+\t\t$versions = array();
+\t\tforeach ( $releases as $release ) {{
+\t\t\tif ( ! empty( $release['draft'] ) || ! empty( $release['prerelease'] ) || empty( $release['tag_name'] ) || empty( $release['assets'] ) || ! is_array( $release['assets'] ) ) {{
+\t\t\t\tcontinue;
+\t\t\t}}
+\t\t\t$version = ltrim( sanitize_text_field( (string) $release['tag_name'] ), 'vV' );
+\t\t\tif ( ! preg_match( '/^\\d+\\.\\d+\\.\\d+$/', $version ) || ! version_compare( $version, {prefix}_VERSION, '<' ) ) {{
+\t\t\t\tcontinue;
+\t\t\t}}
+\t\t\t$expected = '{slug}-' . $version . '.zip';
+\t\t\tforeach ( $release['assets'] as $asset ) {{
+\t\t\t\tif ( isset( $asset['name'], $asset['browser_download_url'] ) && $expected === $asset['name'] ) {{
+\t\t\t\t\t$versions[ $version ] = esc_url_raw( (string) $asset['browser_download_url'] );
+\t\t\t\t\tbreak;
+\t\t\t\t}}
+\t\t\t}}
+\t\t}}
+\t\tuksort(
+\t\t\t$versions,
+\t\t\tstatic function ( $a, $b ) {{
+\t\t\t\treturn version_compare( $b, $a );
+\t\t\t}}
+\t\t);
+\t\tset_transient( $cache_key, $versions, HOUR_IN_SECONDS );
+\t\treturn $versions;
+\t}}
+
 \tpublic function rollback() {{
 \t\tif ( ! current_user_can( 'update_plugins' ) ) {{
 \t\t\twp_die( esc_html( 'Insufficient permissions.' ) );
@@ -98,6 +147,23 @@ final class Rollback_Manager {{
 \t\tif ( ! in_array( $requested, self::list_backups(), true ) ) {{
 \t\t\t$this->redirect( 'rollback_error' );
 \t\t}}
+\t\t$this->perform_rollback( trailingslashit( self::backup_directory() ) . $requested );
+\t}}
+
+\tpublic function rollback_release() {{
+\t\tif ( ! current_user_can( 'update_plugins' ) ) {{
+\t\t\twp_die( esc_html( 'Insufficient permissions.' ) );
+\t\t}}
+\t\tcheck_admin_referer( '{action}_release_rollback' );
+\t\t$requested = isset( $_POST['version'] ) ? sanitize_text_field( wp_unslash( $_POST['version'] ) ) : '';
+\t\t$versions  = self::list_release_versions();
+\t\tif ( ! isset( $versions[ $requested ] ) || ! version_compare( $requested, {prefix}_VERSION, '<' ) ) {{
+\t\t\t$this->redirect( 'rollback_error' );
+\t\t}}
+\t\t$this->perform_rollback( $versions[ $requested ] );
+\t}}
+
+\tprivate function perform_rollback( $package ) {{
 \t\t$current_backup = self::create_code_backup( 'pre-rollback' );
 \t\t$data_snapshot  = Data_Portability::create_snapshot( 'pre-rollback' );
 \t\tif ( is_wp_error( $current_backup ) || is_wp_error( $data_snapshot ) ) {{
@@ -105,10 +171,10 @@ final class Rollback_Manager {{
 \t\t}}
 \t\trequire_once ABSPATH . 'wp-admin/includes/file.php';
 \t\trequire_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
-\t\t$package  = trailingslashit( self::backup_directory() ) . $requested;
 \t\t$upgrader = new \\Plugin_Upgrader( new \\Automatic_Upgrader_Skin() );
 \t\t$result   = $upgrader->install( $package, array( 'overwrite_package' => true ) );
 \t\tdelete_site_transient( 'update_plugins' );
+\t\tdelete_transient( '{action}_release_rollback_versions' );
 \t\tif ( is_wp_error( $result ) || false === $result ) {{
 \t\t\t$this->redirect( 'rollback_error' );
 \t\t}}
