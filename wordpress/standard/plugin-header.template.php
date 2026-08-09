79860626985d664c517de85af5955cb2a86ebda1
Eoingti Lab Inc. <master@eoingti.com>
2026-08-09 12:06:30 +0900

    Add canonical WordPress plugin header template

---
+++ wordpress/standard/plugin-header.template.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: {{PLUGIN_NAME}}
+ * Plugin URI: {{PLUGIN_URI}}
+ * Description: {{PLUGIN_DESCRIPTION}}
+ * Version: {{PLUGIN_VERSION}}
+ * Author: EOINGTI Lab
+ * Author URI: https://eoingti.com/
+ * Text Domain: {{TEXT_DOMAIN}}
+ * Requires at least: {{REQUIRES_WP}}
+ * Requires PHP: {{REQUIRES_PHP}}
+ * Update URI: {{UPDATE_URI}}
+ * License: {{LICENSE}}
+ * License URI: {{LICENSE_URI}}
+ */
+
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+/*
+Canonical repository:
+https://github.com/Eoingtilab/{{PLUGIN_SLUG}}
+
+Metadata rules:
+- {{PLUGIN_URI}}: use the real eoingti.com product/documentation page when it exists.
+  Until then use https://eoingti.com/; never invent a path.
+- {{UPDATE_URI}}: use the product's real custom update identifier/URL when required.
+- Keep Author/Author URI consistent with docs/COMPANY-PROFILE.md.
+*/
